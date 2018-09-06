fb5d18c210abc54933f07d715ce9e41b12722816
playmono <adrgranado@gmail.com>
2018-09-06 16:59:34 +0200

    añadido niveles, mejor performance con kill/revive de sprites, mejorado emoji de guiris

---
+++ js/levels.js
@@ -0,0 +1,65 @@
+levels = [
+    {
+        title: 'Monday',
+        guirisTotalCount: 5,
+        velocity: 1,
+        guirisHappyCount: 0,
+        rubbishCleaned: 0,
+        warningCount: 0,
+        moneySpent: 0
+    }, 
+    {
+        title: 'Tuesday',
+        guirisTotalCount: 10,
+        velocity: 1.5,
+        guirisHappyCount: 0,
+        rubbishCleaned: 0,
+        warningCount: 0,
+        moneySpent: 0
+    },
+    {
+        title: 'Wednesday',
+        guirisTotalCount: 15,
+        velocity: 2,
+        guirisHappyCount: 0,
+        rubbishCleaned: 0,
+        warningCount: 0,
+        moneySpent: 0
+    },
+    {
+        title: 'Thursday',
+        guirisTotalCount: 20,
+        velocity: 2.5,
+        guirisHappyCount: 0,
+        rubbishCleaned: 0,
+        warningCount: 0,
+        moneySpent: 0
+    }, 
+    {
+        title: 'Friday',
+        guirisTotalCount: 25,
+        velocity: 3,
+        guirisHappyCount: 0,
+        rubbishCleaned: 0,
+        warningCount: 0,
+        moneySpent: 0,
+    }, 
+    {
+        title: 'Saturday',
+        guirisTotalCount: 30,
+        velocity: 3.5,
+        guirisHappyCount: 0,
+        rubbishCleaned: 0,
+        warningCount: 0,
+        moneySpent: 0,
+    }, 
+    {
+        title: 'Sunday',
+        guirisTotalCount: 35,
+        velocity: 4,
+        guirisHappyCount: 0,
+        rubbishCleaned: 0,
+        warningCount: 0,
+        moneySpent: 0,
+    }
+];
